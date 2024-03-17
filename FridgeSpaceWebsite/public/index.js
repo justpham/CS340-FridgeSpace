d141690b0a5189fadc7d6ba68b048cee47327327
Justin Pham <phamjus@oregonstate.edu>
2024-03-16 20:22:41 -0700

    Refactored and Styled Website

--- FridgeSpaceWebsite/public/index.js
+++ FridgeSpaceWebsite/public/index.js
@@ -1,17 +1,17 @@
 
-const resetDatabase = document.getElementById('databaseReset')
+// const resetDatabase = document.getElementById('databaseReset')
 
-resetDatabase.addEventListener('mousedown', function(event) {
-    event.preventDefault()
+// resetDatabase.addEventListener('mousedown', function(event) {
+//     event.preventDefault()
 
-    // Send a put request to 
-    fetch('/resetDatabase', {method: 'PUT'})
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-        })
-        .catch(error => {
-            console.error('Error making PUT request:', error);
-        });
-})
+//     // Send a put request to 
+//     fetch('/resetDatabase', {method: 'PUT'})
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error('Network response was not ok');
+//             }
+//         })
+//         .catch(error => {
+//             console.error('Error making PUT request:', error);
+//         });
+// })
